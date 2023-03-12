35868af2226526bf87505aa6bf07954c1b3dca7b
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2023-03-13 02:29:26 +0530

    first commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Home.css'
-import vdo from '../Assets/Loader.gif'
+import vdo from '../Assets/vdo_Qw17Z8lZ (online-video-cutter.com).mp4'
 
 
 
