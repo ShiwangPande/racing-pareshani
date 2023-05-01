0445f8aac06b5e19d7b6c517f35a04aab3f72058
saurav-bondhate <80776924+saurav-bondhate@users.noreply.github.com>
2023-05-01 18:53:33 +0530

    Update Sponsarship.js

--- src/Components/Otherpages/Sponsarship.js
+++ src/Components/Otherpages/Sponsarship.js
@@ -5,6 +5,12 @@ function Sponsarship() {
     <>
       <h1 className='text-center' style={{ color: "red" }}>Current Sponsorship</h1>
       <h1 className='text-center' style={{ color: "red" }}>Previous Sponsorship</h1>
+ <div className="row justify-content-center mt-6">
+                            <div className="col-6 col-md-4 col-lg-3 d-flex flex-center">
+                                <div className=""><img className="logo-grid w-100" src="https://drive.google.com/uc?export=view&id=1HKzqS5ML7V67kTDn72nEDV_oO6w-2wug" alt=""/></div>
+                            </div>
+
+                        </div>
     </>
   )
 }
